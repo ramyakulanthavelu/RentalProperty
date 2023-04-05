351c4595b412812bdeb9913fe5e793b0da971155
karthik30122001 <karthikeyan.vpani@gmail.com>
2023-04-05 16:19:23 +0530

    Prototype

--- prototype/components/Navbar.js
+++ prototype/components/Navbar.js
@@ -1,21 +1,24 @@
 import React from "react";
 import logo from "../public/logo.png";
 import Image from "next/image";
-
+import Link from "next/link";
 function Navbar() {
   return (
     <div className="layout-navbar">
-      <Image class ="img" src={logo} height={40} width={40} />
+      <Image class="img" src={logo} height={40} width={40} alt="Your Company" />
       <nav>
         <ul class="navlinks">
           <li>
-            <a href="#"></a>Home
+            <Link className="link" href="/">Home</Link>
           </li>
           <li>
-            <a href="#"></a>Bout Us
+            <Link className="link" href="/about">Bout Us</Link>
           </li>
           <li>
-            <a href="#"></a>Procuts
+            <Link className="link" href="/products">Products</Link>
+          </li>
+          <li>
+            <Link className="link" href="/login">Login</Link>
           </li>
         </ul>
       </nav>
--- prototype/pages/about.js
+++ prototype/pages/about.js
@@ -11,6 +11,8 @@ function About() {
 
 export default About;
 
+
+{/*
 About.getLayout = function PageLayout(page) {
   return (
     <>
@@ -19,3 +21,4 @@ About.getLayout = function PageLayout(page) {
     </>
   );
 };
+*/}
