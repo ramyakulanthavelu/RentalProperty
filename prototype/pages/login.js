2db7627303e44768acf3e54111c5983dff9588e5
karthik30122001 <karthikeyan.vpani@gmail.com>
2023-04-05 16:26:50 +0530

    prototype

---
+++ prototype/pages/login.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+function login() {
+  return (
+    <div className="login">
+      <div className="sec"><h2>Text</h2></div>
+      <div className="sec"><h2>Text</h2></div>
+      <div className="sec"><h2>Text</h2></div>
+      <div className="sec"><h2>Text</h2></div>
+      <div className="sec"><h2>Text</h2></div>
+      
+    </div>
+  );
+}
+
+
+export default login;
