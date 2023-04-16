4d5fc5185f88a4ffa415e556718e38215c321fe0
karthik30122001 <karthikeyan.vpani@gmail.com>
2023-04-16 20:19:53 +0530

    Removed Prototype

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
-
+import "../styles/layout.css";
+import "../styles/navbar.css";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  if (Component.getLayout) {
+    return Component.getLayout(<Component {...pageProps} />);
+  }
+
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
