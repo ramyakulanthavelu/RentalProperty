5afa54262bfcc08abbbfdfc9c76527caccee9791
priscilla <prathibakvn@gmail.com>
2023-04-17 11:14:02 +0530

    mine too updated

---
+++ nextjs/pages/_app.js
@@ -0,0 +1,6 @@
+import '@/styles/globals.css'
+import '../styles/feedback.css'
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
---
+++ nextjs/pages/signin.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+<main className='flex flex-col items-center justify-center w-full flex-1 px-20 text-center'>
+<div className="bg-white rounded-">
+<div>Sign In</div>
+</div>
+</main>
