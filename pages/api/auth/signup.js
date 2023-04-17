1941f9c43caf0038889e25ee2e179b43b9903037
karthik30122001 <karthikeyan.vpani@gmail.com>
2023-04-17 21:11:58 +0530

    Login + Tailwind

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,29 @@
+import connectMongo from "@/database/conn";
+import Users from "@/model/Schema";
+import { hash } from "bcryptjs";
+
+export default async function handler(req, res) {
+  connectMongo().catch((error) => res.json({ error: "Connection Failed" }));
+
+  if (req.method === "POST") {
+    if (!req.body)
+      return res.status(404).json({ error: "Don't have form data" });
+    const { username, email, password } = req.body;
+
+    const checkexisting = await Users.findOne({ email });
+    if (checkexisting)
+      return res.status(422).json({ message: "User Already Exists" });
+
+    Users.create(
+      { username, email, password: await hash(password, 12) },
+      function (err, data) {
+        if (err) return res.status(404).json({ err });
+        res.status(201).json({ status: true, user: data });
+      }
+    );
+  } else {
+    res
+      .status(500)
+      .json({ message: "HTTPS method not valid only POST acitve" });
+  }
+}
