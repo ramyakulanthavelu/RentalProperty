75275f9c8dc64222be7dd42cb9a77b090f8321cd
rentalprop <veluramya07693@gmail.com>
2023-04-17 11:02:32 +0530

    footer done

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
 
 function Footer() {
   return (
-    <div className='layout-footer'>Footer</div>
+    <div className='footer'>
+      <div className='SocialMedia'>
+        <InstagramIcon/>
+        <TwitterIcon/>
+        <FacebookIcon/>
+        <LinkedInIcon/>
+        <EmailIcon/>
+      </div>
+      <p> &copy; 2023 Swipe Right</p>
+    </div>
   )
 }
 
