2daddba46d2484f250ce2941777470e6c8ba268a
rentalprop <veluramya07693@gmail.com>
2023-04-24 17:54:43 +0530

    gvhmn k :qa

--- nextjs/tailwind.config.js
+++ nextjs/tailwind.config.js
@@ -1,30 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-<<<<<<< HEAD
-const { light } = require('@mui/material/styles/createPalette');
-const defaultTheme = require('tailwindcss/defaultConfig');
-
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  important: true,
-  theme: {
-    ...defaultTheme,
-    colors: {
-      ...defaultTheme.colors,
-      primary: "#3B81F6",
-      white: '#ffffff',
-      text: {
-        DEFAULT: "#1F2937",
-        light: "#6C7281",
-      },
-      light: {
-        DEFAULT: "#FAFBFC",
-        lighter: "#F3F4F6",
-      },
-    },
-=======
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -32,7 +6,6 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
->>>>>>> f376d0c08ae30ca598c895953326c1bf5543bd57
     extend: {},
   },
   plugins: [],
