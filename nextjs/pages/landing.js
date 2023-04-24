043a566777e1d0bbe28b1574b320b849569e1932
rentalprop <veluramya07693@gmail.com>
2023-04-24 11:20:04 +0530

    ggg

--- nextjs/pages/landing.js
+++ nextjs/pages/landing.js
@@ -14,7 +14,7 @@ const sidebar = () => {
         <h1>Welcome</h1>
         <p>This is the main content area.</p>
       </div>
-      <style jsx>{`
+      <style>{`
         .container {
           display: flex;
         }
