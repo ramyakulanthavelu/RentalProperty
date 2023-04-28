c9feb772e03b33955191291c19e2b25a4d139dd0
rentalprop <veluramya07693@gmail.com>
2023-04-28 22:12:15 +0530

    karsouel

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "../styles/products.css";
-import "../styles/Slider.css";
 import "../styles/layout.css";
 import "../styles/navbar.css";
 import "../styles/section.css";
+import '../styles/karsouel.css';
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import NavBarv2 from "@/components/NavBarv2";
-
 
 export default function App({ Component, pageProps }) {
   if (Component.getLayout) {
