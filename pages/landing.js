37a2b902bb6d03e38367c1701a601bfe46210921
karthik30122001 <karthikeyan.vpani@gmail.com>
2023-04-29 19:52:00 +0530

    Session hook

--- pages/landing.js
+++ pages/landing.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { useSession,signOut } from "next-auth/react";
 
-function landing() {
+export default function landing() {
+  const { data: session } = useSession();
+  function handlesignout(){
+    signOut();
+  }
+
   return (
-    <div>landing</div>
-  )
+    <div className="flex justify-center min-h-screen">
+      {session ? login({ session,handlesignout }) : guest()}
+    </div>
+  );
 }
 
-export default landing
+function guest() {
+  return <div className="text-xl text-black">Failed to Login</div>;
+}
+
+function login({ session, handlesignout }) {
+  return (
+    <div className="text-xl text-black">
+      Logged in Ratface
+      <div>
+        <p>{session.user.name}</p>
+      </div>
+      <div className="flex justify-center">
+        <button onClick={handlesignout} className=" py-5 px-10 bg-blue-200 rounded-md">Signout</button>
+      </div>
+    </div>
+  );
+}
