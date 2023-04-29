2fd3139caf8b0e6b2350a98c4af77e9cf6dd7e7b
karthik30122001 <karthikeyan.vpani@gmail.com>
2023-04-29 20:38:46 +0530

    Resigter

--- pages/register.js
+++ pages/register.js
@@ -84,9 +84,9 @@ function register() {
               <input
                 type="password"
                 className="block border border-grey-light w-full p-3 rounded mb-4"
-                name="Password"
-                placeholder="password"
-                {...formik.getFieldProps("password")}
+                name="RPassword"
+                placeholder="Repeat Password"
+                
               />
 
               <button
