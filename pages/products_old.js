809c4c93b2fb53372e04a27a9899e53034ca91be
rentalprop <veluramya07693@gmail.com>
2023-05-10 20:04:52 +0530

    llmm

---
+++ pages/products_old.js
@@ -0,0 +1,52 @@
+import React from "react";
+import Image from "next/image";
+
+
+
+
+
+
+export const getStaticProps = async () => {
+  const res = await fetch("http://localhost:3000/api/properties");
+  //console.log({message : res});
+  const posts = await res.json();
+  //console.log(posts);
+
+  if (!posts) {
+    return {
+      notFound: true,
+    };
+  }
+  //console.log(posts);
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
+
+const products = ({ posts }) => {
+  return (
+    <>
+      <div>
+      <div className="max-w-xs rounded overflow-hidden shadow-lg" style={{ }}>
+        <Image width={400} height={400} className="" src="/img3.jpg" alt="Sunset in the mountains" />
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+          <p className="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+          </p>
+        </div>
+        {/* <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+        </div> */}
+      </div>
+      </div>
+    </>
+  );
+};
+
+export default products;
